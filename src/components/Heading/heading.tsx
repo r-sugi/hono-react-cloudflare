cfd7c925039e523c71351938a3e178bfce073ae1
r-sugi <r.sugimoto0615@gmail.com>
2025-02-24 15:57:57 +0900

    add: tailwind sample

--- src/components/Heading/heading.tsx
+++ src/components/Heading/heading.tsx
@@ -4,14 +4,16 @@
 
 import { Fragment } from "react";
 
-function Heading() {
-  return (
-    <Fragment>
-      <h1>Hello World</h1>
-      <h3>Vite + React + TS + Cloudflare + SSR</h3>
-    </Fragment>
-  );
-}
+const Heading = () => {
+	return (
+		<Fragment>
+			<h1 className="text-4xl font-bold text-blue-500">Hello World!</h1>
+			<h3 className="text-xl text-gray-700">
+				Vite + React + TS + Cloudflare + SSR
+			</h3>
+		</Fragment>
+	);
+};
 
 export default Heading;
 
